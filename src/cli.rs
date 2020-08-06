1d35a1cbdd75e5246a4761d1d7a41680aa47074e
Florian Gamböck <mail@floga.de>
2020-08-06 14:15:48 +0200

    Add file parameter

--- src/cli.rs
+++ src/cli.rs
@@ -1,24 +1,32 @@
 use std::error::Error;
-use std::io;
 
-use clap::{crate_authors, crate_description, crate_name, crate_version, App};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
 use crate::run;
+
+const ARG_FILE: &str = "file";
 
 pub struct Cli;
 
 impl Cli {
     pub fn run() -> Result<(), Box<dyn Error>> {
-        let _arguments = App::new(crate_name!())
+        let arguments = App::new(crate_name!())
             .version(crate_version!())
             .author(crate_authors!())
             .about(crate_description!())
+            .args(&[Arg::with_name(ARG_FILE)
+                .short(&ARG_FILE[0..1])
+                .long(ARG_FILE)
+                .help("The file with the port descriptions, in CSV format")
+                .required(true)
+                .takes_value(true)
+                .number_of_values(1)])
             .get_matches_safe()
             .unwrap_or_else(|e| e.exit());
 
-        let mut rdr = csv::ReaderBuilder::new()
-            .delimiter(b';')
-            .from_reader(io::stdin());
+        let file = arguments.value_of_os(ARG_FILE).unwrap();
+
+        let mut rdr = csv::ReaderBuilder::new().delimiter(b';').from_path(file)?;
 
         for result in rdr.deserialize() {
             run(result?)?;
